7e817cbfe01e2cfc70f4acacd22482c6384ddbdc
Cynthia <ccgatua@gmail.com>
2017-08-10 23:54:34 +0000

    Move new script code into html, added API for San Francisco, includes temp and wind

--- apiexample/script.js
+++ apiexample/script.js
@@ -1,15 +1,9 @@
-$.ajax( {
-    url: "http://api.openweathermap.org/data/2.5/weather",
-    data: {"zip": "02908,us", "appid": "a806a6b5e2943ee6f6d7e4a1d752ffbb"},
-    success: function(response) {
-        console.log(response);
-    }
-})
-$.ajax( {
-    type: "POST",
-    url: "http://api.openweathermap.org/data/2.5/weather",
-    data: {"zip": "02908,us", "appid": "a806a6b5e2943ee6f6d7e4a1d752ffbb"},
-    success: function(response) {
-        console.log(response);
-    }
-})
+var weather = new XMLHttpRequest();
+weather.open("GET", "http://api.wunderground.com/api/1c76987a439f31c6/conditions/q/CA/San_Francisco.json", false);
+weather.send(null);
+
+var r = JSON.parse()(weather.response);
+var dis = "Current location:" + r.current_observation.display_location.full + "<br />";
+var temp = r.current_observation.temperature_string + "<br />";
+var wind = r.current_observation.wind_string + "<br />";
+document.getElementById("weather");
